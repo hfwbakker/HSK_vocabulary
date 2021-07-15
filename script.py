a28f1211695f324cae17c7b33f2665ada315cf44
Henri Bakker <henribakker@apples-MacBook-Air.local>
2021-07-15 23:07:12 +0800

    sessoin 2

--- script.py
+++ script.py
@@ -1,5 +1,6 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 import pandas as pd
 
 df = pd.read_excel("data/HSK_vocabulary.xlsx")
@@ -7,6 +8,9 @@ df = pd.read_excel("data/HSK_vocabulary.xlsx")
 print("Hello world!")
 
 print(df)
+=======
+print("Hello world!")
+>>>>>>> 2f47416920db6977832b21a02adae9c408004d6c
 =======
 print("Hello world!")
 >>>>>>> 2f47416920db6977832b21a02adae9c408004d6c
